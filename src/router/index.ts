1fe7de181eb9f872a346b1ac7d25d77b62f2720a
LanZhan <WANGYIYI06300602@126.com>
2025-12-25 13:05:00 +0800

    初始化项目

---
+++ src/router/index.ts
@@ -0,0 +1,16 @@
+import MainMenuView from "@/views/MainMenuView.vue";
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "main",
+            component: MainMenuView,
+        },
+    ],
+});
+
+export default router;
+
